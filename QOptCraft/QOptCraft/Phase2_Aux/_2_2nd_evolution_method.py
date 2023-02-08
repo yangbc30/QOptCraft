72047c13a5c1a68ac3ea1da634881b75729bca29
Pablo V. Parellada <pablovegan98@icloud.com>
2023-02-08 15:33:43 +0100

    add faster permanent formulas

--- QOptCraft/QOptCraft/Phase2_Aux/_2_2nd_evolution_method.py
+++ QOptCraft/QOptCraft/Phase2_Aux/_2_2nd_evolution_method.py
@@ -26,24 +26,16 @@ import time
 
 # NumPy instalation: in the cmd: 'py -m pip install numpy'
 import numpy as np
-
-
-# ----------FILE MANAGEMENT:----------
-
-# File opening
-from io import open 
+import numba
 
 
 # ----------COMBINATORY:----------
 
 from ..recur_factorial import fact_array
 
-from .ryser_permanent import ryser_loop
 
 
 # ----------PERMUTATIONS:----------
-
-from itertools import permutations
 
 
 # ---------------------------------------------------------------------------------------------------------------------------
@@ -51,59 +43,135 @@ from itertools import permutations
 # ---------------------------------------------------------------------------------------------------------------------------
 
 
-# Permanent computation
-def permanent(M):
+@numba.jit(nopython=True)
+def permanent_ryser(matrix: np.ndarray) -> float or complex:
+    """
+    Returns the permanent of a matrix using the Ryser formula in Gray ordering.
 
-	# We assume square matrix
-	N=len(M)
-	# Permutations array
-	perm_iterator=permutations(range(N))	
-	suma=0.0
+    Parameters
+    ----------
+    matrix : np.ndarray
+        A square matrix
 
-	for item in perm_iterator:
+    Returns
+    -------
+    float or complex : the permanent of the matrix
 
-		# We initialise the product for each addend
-		product=1.0 
-		# Product computation
-		for j in range(N):
+    References
+    ----------
+    The code is based off a Python 2 code from user 'xnor' found in
+    https://codegolf.stackexchange.com/questions/97060/calculate-the-permanent-as-quickly-as-possible
 
-			if N==1:
-				product*=M[j]
-			else:
-				product*=M[j,np.asarray(item)[j]-1]
-		suma+=product
+    Ryser formula with Gray code:
+    Nijenhuis, Albert; Wilf, Herbert S. (1978), Combinatorial Algorithms, Academic Press.
 
-	return suma
+    Ryser formula
+    https://en.wikipedia.org/wiki/Computing_the_permanent#Ryser_formula
+
+    Gray code used in the algorithm
+    https://en.wikipedia.org/wiki/Gray_code
+
+    This algorithm is also re-implemented in The-Walrus library
+    https://github.com/XanaduAI/thewalrus/blob/master/thewalrus/_permanent.py
+    """
+    dim = len(matrix)
+    if dim == 0:
+        return matrix.dtype.type(1.0)
+    # row_comb keeps the sum of previous subsets.
+    row_comb = np.zeros((dim), dtype=matrix.dtype)
+
+    permanent = 0
+    old_grey = 0
+    sign = +1
+
+    binary_power_list = [2**i for i in range(dim)]
+    num_loops = 2**dim
+
+    for bin_index in range(1, num_loops):
+
+        permanent += sign * np.prod(row_comb)
+
+        new_grey = bin_index ^ (bin_index // 2)
+        grey_diff = old_grey ^ new_grey
+        grey_diff_index = binary_power_list.index(grey_diff)
+
+        new_vector = matrix[grey_diff_index]
+        direction = (old_grey > new_grey) - (old_grey < new_grey)  # same as cmp() in python 2
+
+        for i in range(dim):
+            row_comb[i] += new_vector[i] * direction
+
+        sign = -sign
+        old_grey = new_grey
+
+    permanent += sign * np.prod(row_comb)
+
+    return permanent * (-1) ** dim
 
 
-# Permanent computation
-def permanent_ryser(M):
+@numba.jit(nopython=True)
+def permanent(matrix: np.ndarray) -> float or complex:
+    """
+    Returns the permanent of a matrix using the Ryser formula in Gray ordering.
 
-	# We assume square matrix
-	N=len(M)
-	# Sum initialisation 
-	suma=0.0
+    Parameters
+    ----------
+    matrix : np.ndarray
+        A square matrix
 
-	for S in range(N):
-		suma1=0.0
+    Returns
+    -------
+    float or complex : the permanent of the matrix
 
-		# Combs
-		combs=ryser_loop(N,S)
-		for i in range(len(combs)):
-			# We initialise the product for each addend
-			product=1.0 
-			# Product computation
-			for j in range(N):
-				# Sum initialisation (2)
-				suma2=0.0
-				for k in range(N-S): # compute columns
-					suma2+=M[j,combs[i,k]]
-				product*=suma2
-			suma1+=product
-		suma1*=np.power(-1,np.abs(S))
-		suma+=suma1
+    References
+    ----------
+    The code is based off a Python 2 code from user 'xnor' found in
+    https://codegolf.stackexchange.com/questions/97060/calculate-the-permanent-as-quickly-as-possible
 
-	return np.power(-1,N)*suma
+    Glynn algorithm for the permanent
+    http://library.isical.ac.in:8080/jspui/bitstream/10263/3603/1/TH73.pdf
+
+    Gray code used in the algorithm
+    https://en.wikipedia.org/wiki/Gray_code
+
+    This algorithm is also re-implemented in The-Walrus library
+    https://github.com/XanaduAI/thewalrus/blob/master/thewalrus/_permanent.py
+    """
+    dim = len(matrix)
+    if dim == 0:
+        return matrix.dtype.type(1.0)
+    # sum of each row of the matrix
+    row_comb = np.sum(matrix, axis=0)
+
+    permanent = 0
+    old_grey = 0
+    sign = +1
+
+    binary_power_list = [2**i for i in range(dim)]
+    num_loops = 2 ** (dim - 1)
+
+    for bin_index in range(1, num_loops):
+
+        permanent += sign * np.prod(row_comb)
+
+        new_grey = bin_index ^ (bin_index // 2)
+        grey_diff = old_grey ^ new_grey
+        grey_diff_index = binary_power_list.index(grey_diff)
+
+        new_vector = matrix[grey_diff_index]
+        direction = 2 * (old_grey > new_grey) - 2 * (old_grey < new_grey)  # same as cmp() in python 2
+
+        for i in range(dim):
+            row_comb[i] += new_vector[i] * direction
+
+        sign = -sign
+        old_grey = new_grey
+
+    permanent += sign * np.prod(row_comb)
+
+    return (-1) ** dim * permanent / num_loops
+
+
 
 
 # Submatrices
