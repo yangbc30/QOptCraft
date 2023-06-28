f7f8c70e2de422096220aa8f3748dc81079ef831
Pablo <pablovegan98@icloud.com>
2023-06-28 12:50:05 +0200

    fixed .basis import?

--- QOptCraft/basis/__init__.py
+++ QOptCraft/basis/__init__.py
@@ -1,3 +1,4 @@
 from .photon import get_photon_basis, state_in_basis, BasisPhoton
 from .algebra import get_algebra_basis, BasisAlgebra
 from .hilbert_dimension import hilbert_dim
+from .config import FOLDER_PATH
