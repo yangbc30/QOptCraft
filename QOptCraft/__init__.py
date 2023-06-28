4bf4a9820cd18ad5af6b3b0f5bc5059e67f3d152
Pablo <pablovegan98@icloud.com>
2023-06-28 12:58:03 +0200

    up

--- QOptCraft/__init__.py
+++ QOptCraft/__init__.py
@@ -1,5 +1,5 @@
 from ._version import __version__
 
-from .basis import get_algebra_basis, get_photon_basis
-from .state import State, PureState, MixedState
+# from .basis import get_algebra_basis, get_photon_basis
+from .state import Fock, State, PureState, MixedState
 from .qoptcraft import QOptCraft
