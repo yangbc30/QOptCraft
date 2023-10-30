80b9c34f6bb4c5b143da01a3d5dead4dea2fb71d
Pablo <pablovegan98@icloud.com>
2023-10-30 13:02:05 +0100

    modify spectral invariant as complement basis is not really needed

--- qoptcraft/invariant/spectral_invariant.py
+++ qoptcraft/invariant/spectral_invariant.py
@@ -8,17 +8,16 @@ from qoptcraft.math import Matrix, hs_scalar_product
 
 
 def projection_density(state: State, subspace: Literal["image", "complement"]) -> Matrix:
-    if subspace == "image":
-        basis = basis_image_orthonormal(state.modes, state.photons)
-    elif subspace == "complement":
-        basis = basis_complement_image_orthonormal(state.modes, state.photons)
-    else:
-        raise ValueError("Supported options for the subspace are 'image' and 'complement'.")
+    basis_image = basis_image_orthonormal(state.modes, state.photons)
     matrix = 1j * state.density_matrix
-    projection = np.zeros_like(matrix)
-    for basis_matrix in basis:
-        projection += hs_scalar_product(basis_matrix, matrix) * basis_matrix
-    return projection
+    projection_image = np.zeros_like(matrix)
+    for basis_matrix in basis_image:
+        projection_image += hs_scalar_product(basis_matrix, matrix) * basis_matrix
+    if subspace == "image":
+        return projection_image
+    if subspace == "complement":
+        return state.density_matrix - projection_image
+    raise ValueError("Supported options for the subspace are 'image' and 'complement'.")
 
 
 def spectral_invariant(
