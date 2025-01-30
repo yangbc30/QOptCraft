f8558b4498bf6866d87596097b3b894fb3b5ab69
pablovegan <pablovegan98@icloud.com>
2025-01-30 11:36:24 +0100

    real eigenvalues

--- qoptcraft/invariant/spectral_invariant.py
+++ qoptcraft/invariant/spectral_invariant.py
@@ -20,10 +20,10 @@ def spectral_invariant(
     """
     if subspace == "image":
         projection = projection_density(state, subspace="image", orthonormal=orthonormal)
-        return np.linalg.eigvals(projection).imag * 1j
+        return np.linalg.eigvals(projection).imag
     elif subspace == "complement":
         projection = projection_density(state, subspace="complement", orthonormal=orthonormal)
-        return np.linalg.eigvals(projection).imag * 1j
+        return np.linalg.eigvals(projection).imag
     elif subspace == "full":
         projection_image = projection_density(state, subspace="image", orthonormal=orthonormal)
         projection_complement = projection_density(
