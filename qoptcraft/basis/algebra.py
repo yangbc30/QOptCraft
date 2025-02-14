fix calculation of S from U

qoptcraft/basis/algebra.py
@@ -2,6 +2,7 @@ import pickle
 import warnings
 
 import numpy as np
+from numpy.typing import NDArray
 from scipy.sparse import spmatrix, lil_matrix
 
 from .photon import photon_basis, BasisPhoton
@@ -88,8 +89,8 @@ def image_algebra_basis(modes: int, photons: int, cache: bool = True) -> BasisAl
     return basis
 
 
-def sym_matrix(mode_1: int, mode_2: int, dim: int) -> spmatrix:
-    """Create the element of the algebra i/2(|j><k| + |k><j|)."""
+def sym_matrix(mode_1: int, mode_2: int, dim: int) -> NDArray:
+    """Create the element of the algebra i/sqrt(2)(|j><k| + |k><j|)."""
     matrix = np.zeros((dim, dim), dtype=np.complex128)
     if mode_1 == mode_2:
         matrix[mode_1, mode_1] = 1j
@@ -99,8 +100,8 @@ def sym_matrix(mode_1: int, mode_2: int, dim: int) -> spmatrix:
     return matrix
 
 
-def antisym_matrix(mode_1: int, mode_2: int, dim: int) -> spmatrix:
-    """Create the element of the algebra 1/2(|j><k| - |k><j|)."""
+def antisym_matrix(mode_1: int, mode_2: int, dim: int) -> NDArray:
+    """Create the element of the algebra 1/sqrt(2)(|j><k| - |k><j|)."""
     matrix = np.zeros((dim, dim), dtype=np.complex128)
     matrix[mode_1, mode_2] = SQRT_2_INV
     matrix[mode_2, mode_1] = -SQRT_2_INV

qoptcraft/evolution/inverse_problem.py
@@ -46,8 +46,6 @@ def scattering_from_unitary(unitary: NDArray, modes: int, photons: int) -> NDArr
         for j in range(modes):
             basis_matrix = sym_matrix(j, j, modes)
             for l in range(modes):
-                # ad_matrix = adjoint(basis_matrix)
-                # exp_val = np.einsum("i,ij,j->", identity[l, :], ad_matrix, identity[l, :])
                 exp_val = adjoint(basis_matrix)[l,l]
                 if not np.isclose(exp_val, 0, rtol=1e-5, atol=1e-8):
                     j0, l0 = j, l
@@ -59,19 +57,13 @@ def scattering_from_unitary(unitary: NDArray, modes: int, photons: int) -> NDArr
     scattering = np.zeros((modes, modes), dtype=np.complex128)
     for l in range(modes):
         for j in range(modes):
-            ad_sym = adjoint(sym_matrix(j, j0, modes))
-            sym_term = np.einsum("i,ij,j->", identity[l, :], ad_sym, identity[l0, :])
+            sym_term = adjoint(sym_matrix(j, j0, modes))[l,l0]
             if j != j0:
-                ad_antisym = adjoint(antisym_matrix(j, j0, modes))
-                antisym_term = np.einsum(
-                    "i,ij,j->",
-                    identity[l, :],
-                    ad_antisym,
-                    identity[l0, :],
-                )
+                antisym_term = adjoint(antisym_matrix(j, j0, modes))[l, l0]
+                # ! The factor np.sqrt(2) / 2 is to transform the basis into the one in the paper
+                scattering[l, j] = (antisym_term - 1j * sym_term) / coef * np.sqrt(2) / 2
             else:
-                antisym_term = 0
-            scattering[l, j] = (antisym_term - 1j * sym_term) / coef
+                scattering[l, j] = - 1j * sym_term / coef
     return scattering
 
 