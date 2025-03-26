9cbe271f3c49364ac6d9aa211f1553b1bda2c4fd
pablovegan <pablovegan98@icloud.com>
2025-03-26 17:59:49 +0100

    fix imports

--- qoptcraft/basis/__init__.py
+++ qoptcraft/basis/__init__.py
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 from .photon import photon_basis, BasisPhoton
 from .algebra_basis import (
-=======
-from .photon import BasisPhoton, photon_basis, complete_photon_basis
-from .algebra import (
->>>>>>> 7e9c6625d91239d7cebe17e8ad0ab80ac3e970e2
     BasisAlgebra,
     unitary_algebra_basis,
     image_algebra_basis,
-    complement_algebra_basis_orthonormal,
+    complement_algebra_basis_orthonormal
 )
 from .hilbert_dimension import hilbert_dim
 from .algebra_hm import image_matrix_from_coefs, algebra_hm
